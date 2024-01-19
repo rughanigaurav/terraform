58c1ac533d5f409cb21341a53827ceeab109c49e
darshan-prolix <darshan.prolix@gmail.com>
2024-01-19 19:42:15 +0530

    changes in s3 bucket

--- s3.tf
+++ s3.tf
@@ -7,6 +7,14 @@ resource "aws_s3_bucket" "Linx-s3" {
       
       enabled = false
     }
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+          
+          sse_algorithm = "AES256"
+        }
+      }
+    }
 
     tags = {
       Name="s3_bucket" 
