cd1c84de8d7218c825238d1146dcfc86caf63533
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-02-09 10:46:17 +0530

    Auther changed in s3 file in S3 file

---
+++ s3.tf
@@ -0,0 +1,82 @@
+resource "aws_s3_bucket" "Linx-s3" {
+
+    bucket = "test-terraform191091"
+    acl = "public-read"
+
+    versioning {
+      
+      enabled = false
+    }
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+        
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+    tags = {
+      Name="s3_bucket" 
+    } 
+}
+
+
+resource "aws_s3_bucket" "access_logs" {
+
+    bucket = "test-terraform191091"
+    acl = "public-read"
+
+    versioning {
+      
+      enabled = false
+    }
+    server_side_encryption_configuration {
+      rule {
+        apply_server_side_encryption_by_default {
+       
+          sse_algorithm = "AES256"
+        }
+      }
+    }
+    tags = {
+      Name="accesslog" 
+    } 
+}
+ data "aws_iam_policy_document" "s3_bucket_lb_write" {
+  policy_id = "s3_bucket_lb_logs"
+
+  statement {
+    actions = [
+      "s3:PutObject",
+    ]
+    effect = "Allow"
+    resources = [
+      "${aws_s3_bucket.access_logs.arn}/*",
+    ]
+
+  }
+
+  statement {
+    actions = [
+      "s3:PutObject"
+    ]
+    effect = "Allow"
+    resources = ["${aws_s3_bucket.access_logs.arn}/*"]
+    principals {
+      identifiers = ["delivery.logs.amazonaws.com"]
+      type        = "Service"
+    }
+  }
+
+
+  statement {
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+    effect = "Allow"
+    resources = ["${aws_s3_bucket.access_logs.arn}"]
+
+  }
+}
+
+
