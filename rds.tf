0e05c048a8b6cabc5205ea0f61d0dbc3b430da34
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-02-09 10:37:13 +0530

    Auther changed in rds

--- rds.tf
+++ rds.tf
@@ -1,4 +1,13 @@
-
+resource "aws_db_subnet_group" "db-subnet-group" {
+    
+    name = "kg-subnet"
+    subnet_ids = [aws_route_table_association.private_subnet_1_route_table_association,aws_subnet.private_app_subnet_az2,aws_subnet.public_subnet_az1,aws_subnet.public_subnet_az2 ]
+    description = "Subnet for RDS"
+    tags = {
+      
+      Name = "default-vpc-0217757ec8c0923c7"
+    } 
+}
 resource "aws_db_instance" "db-instance" {
     
     instance_class = "${var.databae-instance-class}"
