a589afaa50e9cd6e1c844818ad80737527a6eb78
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-02-09 10:34:11 +0530

    change in rds to change auther

--- rds.tf
+++ rds.tf
@@ -8,21 +8,3 @@ resource "aws_db_subnet_group" "db-subnet-group" {
       Name = "default-vpc-0217757ec8c0923c7"
     } 
 }
-resource "aws_db_instance" "db-instance" {
-    
-    instance_class = "${var.databae-instance-class}"
-    availability_zone = "us-east-1b"
-    identifier = "${var.database-instance-identifier}"
-    db_subnet_group_name = aws_db_subnet_group.db-subnet-group
-    multi_az = false
-    vpc_security_group_ids = [ aws_security_group.security_group.id ]
-    username = "Pr0l1x_Admin"
-    password = "2tVkHZ4cYaUo66DLRinM"
-    allocated_storage = 20
-    storage_type = "gp2"
-    engine = "postgres"
-    engine_version = 15.5
-    performance_insights_enabled = false
-    port = 3306
-    publicly_accessible = true
-}
