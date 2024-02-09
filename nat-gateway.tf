9c011babebb0ac74c1eb213562d26b0940146e1b
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-02-09 10:50:07 +0530

    Auther changed in nat-gateway file

---
+++ nat-gateway.tf
@@ -0,0 +1,32 @@
+resource "aws_nat_gateway" "KG-Tiger-NAT" {
+  subnet_id = aws_subnet.public_subnet_az1
+  allocation_id = aws_eip.NAT_eip
+tags = {
+
+    Name = "KG-Tiger-NAT" 
+}
+}
+resource "aws_route_table" "Private_RT" {
+    vpc_id = aws_vpc.vpc.id
+
+    route = [{
+
+        cidr_block  = "0.0.0.0/0"
+        gateway_id  = aws_nat_gateway.KG-Tiger-NAT.id
+        carrier_gateway_id         = ""
+        destination_prefix_list_id = ""
+        egress_only_gateway_id     = ""
+        instance_id                = ""
+        ipv6_cidr_block            = ""
+        local_gateway_id           = ""
+        nat_gateway_id             = ""
+        network_interface_id       = ""
+        transit_gateway_id         = ""
+        vpc_endpoint_id            = ""
+        vpc_peering_connection_id  = ""
+}]
+    tags = {
+
+        Name ="Private Route table"
+    } 
+}
