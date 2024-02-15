5df21aaa56f9ffbb99a5293fe5d1c515c81fb82e
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-02-15 17:27:43 +0530

    Changes in ec2 file for syntax issue.

--- ec2.tf
+++ ec2.tf
@@ -31,9 +31,9 @@ resource "aws_instance" "Backend-S1" {
 
     ami = "ami-0fc5d935ebf8bc3bc"
     instance_type = "t2.micro"
-    security_groups = "aws_security_group.security_group"
+    security_groups = aws_security_group.security_group
     key_name = "linx-staging"
-    subnet_id = "aws_subnet.private_app_subnet_az1"
+    subnet_id = aws_subnet.private_app_subnet_az1
   tags = {
     Name = "Backend-S1"
   }
