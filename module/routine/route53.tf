9e369d7ae88b88c9c9511250c5bd950c68663035
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-02-22 11:21:08 +0530

    Added Module of routine and added route53 and it's variables

---
+++ module/routine/route53.tf
@@ -0,0 +1,19 @@
+resource "aws_route53_zone" "Domain-name"{
+    
+    name = var.domain-name
+
+}
+resource "aws_route53_record" "Recordset" {
+  
+  zone_id = aws_route53_zone.Domain-name.id
+  name = var.record-name
+  type = "A"
+alias {
+  
+name = aws_lb.Frontend_alb.dns_name
+zone_id = aws_lb.Frontend_alb.zone_id
+evaluate_target_health = true
+  
+}
+
+}
