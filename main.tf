d0dfdd87955833ab99e477a12c4b9d64923b8763
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-03-28 18:49:33 +0530

    Changes in moudles and vpc file

--- main.tf
+++ main.tf
@@ -1,10 +1,6 @@
- terraform {
-   required_providers {
-     aws = {         
-         source = "hashicorp/aws"
-         version = "~> 4.0 "
-     }
+provider "aws" {
+    region = "us-east-1" 
+    access_key = "AKIAYS2NSI32E4NPK352"
+    secret_key = "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"
 
-   }
-
- }
+}
--- module/vpc/vpc.tf
+++ module/vpc/vpc.tf
@@ -1,50 +1,48 @@
 # create vpc
-# terraform aws linx create vpc for linx
+# terraform aws create vpc for prolix
 resource "aws_vpc" "vpc" {
-  cidr_block              = var.vpc_cidr_block
-  instance_tenancy        = "default"
-  enable_dns_hostnames    = true
+  cidr_block             = var.vpc_cidr_block
+  instance_tenancy       = "default"
+  enable_dns_hostnames   = true
 
-  tags      = {
-    Name    = "test-vpc"
+  tags   = {
+    Name = "test-vpc"
   }
 }
 
-# terraform aws linx create internet gateway
+# terraform aws create internet gateway
 resource "aws_internet_gateway" "internet_gateway" {
   vpc_id    = aws_vpc.vpc.id
 
-  tags      = {
-    Name    = "test-IG"
+  tags       = {
+    Name     = "test-IG"
   }
 }
 
-# create public subnet az1
-# terraform aws linx create subnet
+# terraform aws create subnet
 resource "aws_subnet" "public_subnet_az1" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet_az1_cidr
-  availability_zone       = "us-east-1a"
-  map_public_ip_on_launch = true
-  tags      = {
-    Name    = "public-subnet az1"
+  vpc_id                    = aws_vpc.vpc.id
+  cidr_block                = var.public_subnet_az1_cidr
+  availability_zone         = "us-east-1a"
+  map_public_ip_on_launch   = true
+  tags        = {
+    Name      = "public-subnet az1"
   }
 }
 
 # create public subnet az2
-# terraform aws linx create subnet
-resource "aws_subnet" "public_subnet_az2" {
-  vpc_id                  = aws_vpc.vpc.id
-  cidr_block              = var.public_subnet_az2_cidr
-  availability_zone       = "us-east-1b"
-  map_public_ip_on_launch = true
+resource "aws_subnet" "public_subnet_az2"  {
+vpc_id                      = aws_vpc.vpc.id 
+cidr_block                  = var.public_subnet_az2_cidr
+availability_zone           = "us-east-1b"
+map_public_ip_on_launch     = true
 
     tags      = {
     Name    = "public-subnet az2"
   }
 }
 
-# terraform aws linx create route table
+# terraform aws create route table
 resource "aws_route_table" "public_route_table" {
   vpc_id       = aws_vpc.vpc.id
 
@@ -58,19 +56,19 @@ resource "aws_route_table" "public_route_table" {
   }
 }
 
-# terraform aws linx associate subnet with route table
+# terraform aws associate subnet with route table
 resource "aws_route_table_association" "public_subnet_az1_route_table_association" {
   subnet_id           = aws_subnet.public_subnet_az1.id
   route_table_id      = aws_route_table.public_route_table.id
 }
 
-# terraform aws linx associate subnet with route table
+# terraform aws associate subnet with route table
 resource "aws_route_table_association" "public_subnet_2_route_table_association" {
   subnet_id           = aws_subnet.public_subnet_az2.id
   route_table_id      = aws_route_table.public_route_table.id
 }
 
-# terraform aws linx create subnet
+# terraform aws create subnet
 resource "aws_subnet" "private_app_subnet_az1" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = var.private_subnet_az1_cidr
@@ -82,7 +80,7 @@ resource "aws_subnet" "private_app_subnet_az1" {
   }
 }
 
-# terraform aws linx create subnet
+# terraform aws create subnet
 resource "aws_subnet" "private_app_subnet_az2" {
   vpc_id                   = aws_vpc.vpc.id
   cidr_block               = var.private_subnet_az2_cidr
@@ -94,7 +92,6 @@ resource "aws_subnet" "private_app_subnet_az2" {
   }
 }
 
-#terraform aws linx associate subnet to route_table
 resource "aws_route_table_association" "private_subnet_1_route_table_association" {
 
     subnet_id = aws_subnet.private_app_subnet_az1.id
