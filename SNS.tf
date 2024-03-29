f52f5730eadc3c0b65a0401f1dd599375ce28390
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-03-29 18:49:49 +0530

    SNS.tf

--- SNS.tf
+++ SNS.tf
@@ -1,12 +1,12 @@
-# resource "aws_sns_topic" "sns" {
-#   name = "test-Notification"  
-# }
-# data "aws_sns_topic" "sns" {
-#     name = "test-Notification"
+resource "aws_sns_topic" "sns" {
+  name = "test-Notification"  
+}
+data "aws_sns_topic" "sns" {
+    name = "test-Notification"
 
-# }
-# resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-#   topic_arn = data.aws_sns_topic.sns.arn
-#   protocol  = "email"
-#   endpoint  = "gaurav.prolix@gmail.com"
-# }
+}
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = data.aws_sns_topic.sns.arn
+  protocol  = "email"
+  endpoint  = "gaurav.prolix@gmail.com"
+}
