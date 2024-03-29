e1f86b9d28525746b78a26735e5ae7688805f411
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-03-29 18:51:25 +0530

    main.tf remove committed lines

--- main.tf
+++ main.tf
@@ -5,30 +5,30 @@ provider "aws" {
 
 }
 
-# module "vpc" {
+module "vpc" {
 
-#   source                      = "../modules/vpc"
-#   region                      = us-east-1
-#   vpc_cidr                    = var.vpc_cidr_block
-#   public_subnet_az1_cidr      = var.public_subnet_az1
-#   public_subnet_az2_cidr      = var.public_subnet_az2
-#   private_subnet_az1_cidr     = var.private_app_subnet_az1
-#   private_subnet_az2_cidr     = var.private_app_subnet_az2
+  source                      = "../modules/vpc"
+  region                      = us-east-1
+  vpc_cidr                    = var.vpc_cidr_block
+  public_subnet_az1_cidr      = var.public_subnet_az1
+  public_subnet_az2_cidr      = var.public_subnet_az2
+  private_subnet_az1_cidr     = var.private_app_subnet_az1
+  private_subnet_az2_cidr     = var.private_app_subnet_az2
 
-# }
+}
 
-# module "security_group" {
+module "security_group" {
 
-#   source = "../modules/vpc"
-#   vpc_id = var.vpc_id
+  source = "../modules/vpc"
+  vpc_id = var.vpc_id
 
-# }
+}
 
 
-# module "aws_s3_bucket" {
+module "aws_s3_bucket" {
 
-#   source = "../modules/s3-bucket"
-#   bucket = var.bucket_name
-# }
+  source = "../modules/s3-bucket"
+  bucket = var.bucket_name
+}
 
 
