c72a8fab19224792bd54827f722912d994f72b11
gaurav.rughani@gmail.com <130984781+rughanigaurav@users.noreply.github.com>
2024-03-29 17:58:29 +0530

    File allocation to modules and changes as per required.

--- main.tf
+++ main.tf
@@ -4,3 +4,31 @@ provider "aws" {
     secret_key = "SQvU+hUH7Ca7FHFgyou6LqqW+Dq0xusN6KXqahev"
 
 }
+
+# module "vpc" {
+
+#   source                      = "../modules/vpc"
+#   region                      = us-east-1
+#   vpc_cidr                    = var.vpc_cidr_block
+#   public_subnet_az1_cidr      = var.public_subnet_az1
+#   public_subnet_az2_cidr      = var.public_subnet_az2
+#   private_subnet_az1_cidr     = var.private_app_subnet_az1
+#   private_subnet_az2_cidr     = var.private_app_subnet_az2
+
+# }
+
+# module "security_group" {
+
+#   source = "../modules/vpc"
+#   vpc_id = var.vpc_id
+
+# }
+
+
+# module "aws_s3_bucket" {
+
+#   source = "../modules/s3-bucket"
+#   bucket = var.bucket_name
+# }
+
+
--- s3.tf
+++ s3.tf
@@ -1,66 +1,95 @@
-resource "aws_s3_bucket" "Linx-s3" {
-
-    bucket = "test-terraform191091"
-    acl = "public-read"
-
-    versioning {
-      
-      enabled = false
-    }
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-        
-          sse_algorithm = "AES256"
-        }
-      }
-    }
-    tags = {
-      Name="s3_bucket" 
-    } 
+resource "aws_kms_key" "mykey" {
+  description             = "This key is used to encrypt bucket objects"
+  deletion_window_in_days = 10
 }
+
+resource "aws_s3_bucket" "mybucket" {
+
+  bucket = "mybucket"
+
+  tags = {
+      
+      Name="s3_bucket" 
+  
+  } 
+
+}
+
+  resource "aws_s3_bucket_acl" "example" {
+  bucket = aws_s3_bucket.mybucket.id
+  acl    = "private"
+}
+    
+    
+
+    resource "aws_s3_bucket_versioning" "versioning" {
+
+    bucket = aws_s3_bucket.mybucket.id  
+    versioning_configuration {
+    status = "Enabled"
+  }
+  
+  }
+    
+
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "test" {
+
+    bucket = aws_s3_bucket.mybucket.id
+
+    rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+}
+ 
 
 resource "aws_s3_bucket" "access_logs" {
 
-    bucket = "test-terraform191091"
-    acl = "public-read"
-
-    versioning {
-      
-      enabled = false
-    }
-    server_side_encryption_configuration {
-      rule {
-        apply_server_side_encryption_by_default {
-       
-          sse_algorithm = "AES256"
-        }
-      }
-    }
-
-    lifecycle_rule {
-      enabled = true
-      prefix = ""
-    
-    
-    transition {
-      days = "90"
-      storage_class = "STANDARD_IA"
-    }
-
-    expiration {
-      
-      days = "90"
-      
-    }
-    }
-
+    bucket = "access_logs"
     
     tags = {
+    
       Name="accesslog" 
-    } 
+    
+    }
 }
- data "aws_iam_policy_document" "s3_bucket_lb_write" {
+resource "aws_s3_bucket_lifecycle_configuration" "lifecycle1" {
+    
+    bucket = aws_s3_bucket.mybucket.id
+    
+
+
+}
+      
+    
+resource "aws_s3_bucket_versioning" "versioning2" {
+        
+        bucket = aws_s3_bucket.access_logs.id
+
+        versioning_configuration {
+          status = "Enabled"
+        }
+        
+      }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "example2" {
+
+        bucket = aws_s3_bucket.access_logs.id
+
+        rule {
+    apply_server_side_encryption_by_default {
+      kms_master_key_id = aws_kms_key.mykey.arn
+      sse_algorithm     = "aws:kms"
+    }
+  }
+
+}  
+    
+data "aws_iam_policy_document" "s3_bucket_lb_write" {
   policy_id = "s3_bucket_lb_logs"
 
   statement {
